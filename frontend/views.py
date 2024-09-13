4ecd8cae15280a2444ae173effa1ca0c67fc97e4
pruthimayank <pruthimayank2003@gmail.com>
2024-09-14 01:43:08 +0530

    models fix

--- frontend/views.py
+++ frontend/views.py
@@ -8,5 +8,14 @@ def home(request):
     data = {
         'packages': packages_list
     }
-
+    # print(data)
     return render(request, 'home.html', data)
+
+def package(request, package):
+    package = Packages.objects.filter(name=package)
+    packages_list = list(package.values())
+    data = {
+        'package': packages_list[0]
+    }
+    print(data, "package")
+    return render(request, 'package.html', data)
