c1701e4939a103c11ffe409b8f2c16bdf53bc134
pruthimayank <pruthimayank2003@gmail.com>
2024-09-14 01:31:13 +0530

    model created

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Packages(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    video = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Packages
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    packages = Packages.objects.all()
+    packages_list = list(packages.values())
+    data = {
+        'packages': packages_list
+    }
+
+    return render(request, 'home.html', data)
