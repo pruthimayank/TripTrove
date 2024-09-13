61af62ea03f51a1cc571a93399941835a0b48a0e
pruthimayank <pruthimayank2003@gmail.com>
2024-09-14 02:33:52 +0530

    hello

--- frontend/models.py
+++ frontend/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Packages(models.Model):
     name = models.CharField(max_length=100)
-    image = models.CharField(max_length=100)
-    video = models.CharField(max_length=100)
+    image = models.CharField(max_length=1000)
+    video = models.CharField(max_length=1000)
     description = models.TextField()
 
     def __str__(self):
