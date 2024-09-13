05a92c1a35b4ff1749f7266a84a8362fc064573b
pruthimayank <pruthimayank2003@gmail.com>
2024-09-13 22:35:12 +0530

    vieo added

--- triptrove/settings.py
+++ triptrove/settings.py
@@ -118,6 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # This allows you to place your static files inside the /static folder
+]
+
+# The directory where static files will be collected when you run collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media files (for file uploads, if you have any)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
