a122c063389675059fecb5aa451304814ce52d6a
pruthimayank <pruthimayank2003@gmail.com>
2024-09-16 00:49:51 +0530

    .

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
     path('', views.home, name='home'),
     path('package/<str:package>/', views.package, name='package'),
+    path('packages/', views.packages, name='packages'),
     path('bookings/', views.bookings, name='bookings'),
     path('about/', views.about, name='about'),
 ]
